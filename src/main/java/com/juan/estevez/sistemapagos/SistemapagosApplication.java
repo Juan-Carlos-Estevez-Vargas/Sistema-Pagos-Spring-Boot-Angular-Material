85d4b2e178403babfb38fb2f7e10b09a2a0b9ee4
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2026-01-17 20:32:35 -0500

    Creación del proyecto

---
+++ src/main/java/com/juan/estevez/sistemapagos/SistemapagosApplication.java
@@ -0,0 +1,13 @@
+package com.juan.estevez.sistemapagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemapagosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/estevez/sistemapagos/SistemapagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.estevez.sistemapagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemapagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
