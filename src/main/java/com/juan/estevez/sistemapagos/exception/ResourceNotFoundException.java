5cf871f31b2ed47c0b42778c4811bcaa8210b56a
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2026-01-22 17:27:20 -0500

    Limpia el proyecto

--- src/main/java/com/juan/estevez/sistemapagos/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.juan.estevez.sistemapagos.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
