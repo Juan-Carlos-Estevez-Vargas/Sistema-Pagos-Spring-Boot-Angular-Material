08141aa319770c7a77fafabaa831244629737e1d
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2026-02-01 21:33:59 -0500

    Agrega servicio API
    Este servicio se encarga de hacer peticiones al backend de manera genérica, se usa para todos los componentes solo que centralizado

---
+++ sistema-pagos-frontend/src/app/core/services/api.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private apiUrl = environment.apiUrl;
+  private token = localStorage.getItem("token");
+
+  constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      "content-type": "application/json",
+      "authorization": this.token ? `Bearer ${this.token}`: ''
+    });
+  }
+
+  private getMultipartHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      "authorization": this.token ? `Bearer ${this.token}`: ''
+    })
+  }
+
+  // Métodos genéricos
+  get<T>(endpoint: string, params?: any): Observable<T> {
+    let httpParams = new HttpParams();
+    if (params) {
+      Object.keys(params).forEach(key => {
+        if (params[key] !== null && params[key] !== undefined) {
+          httpParams = httpParams.set(key, params[key]);
+        }
+      });
+    }
+
+    return this.http.get<T>(`${this.apiUrl}${endpoint}`, {
+      headers: this.getHeaders(),
+      params: httpParams
+    });
+  }
+
+  post<T>(endpoint: string, body: any): Observable<T> {
+    return this.http.post<T>(`${this.apiUrl}${endpoint}`, body, {
+      headers: this.getHeaders()
+    });
+  }
+
+  postMultipart<T>(endpoint: string, formData: FormData): Observable<T> {
+    return this.http.post<T>(`${this.apiUrl}${endpoint}`, formData, {
+      headers: this.getHeaders()
+    });
+  }
+
+  put<T>(endpoint: string, body: any): Observable<T> {
+    return this.http.put<T>(`${this.apiUrl}${endpoint}`, body, {
+      headers: this.getHeaders()
+    });
+  }
+
+  patch<T>(endpoint: string, body?: any): Observable<T> {
+    return this.http.patch<T>(`${this.apiUrl}${endpoint}`, body, {
+      headers: this.getHeaders()
+    });
+  }
+
+  delete<T>(endpoint: string): Observable<T> {
+    return this.http.delete<T>(`${this.apiUrl}${endpoint}`, {
+      headers: this.getHeaders()
+    });
+  }
+
+  downloadFile(endpoint: string): Observable<Blob> {
+    return this.http.get(`${this.apiUrl}${endpoint}`, {
+      headers: this.getHeaders(),
+      responseType: 'blob'
+    });
+  }
+
+}
