851b1898318d5b0e9802a31d35c378ae49ec6314
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2026-02-03 19:25:46 -0500

    Agrega componente angular para el loading-spinner de la app

---
+++ sistema-pagos-frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner.component',
+  imports: [],
+  templateUrl: './loading-spinner.component.html',
+  styleUrl: './loading-spinner.component.scss',
+})
+export class LoadingSpinnerComponent {
+
+  @Input() message: string = 'Cargando...';
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input() type: 'inline' | 'overlay' = 'inline';
+
+}
