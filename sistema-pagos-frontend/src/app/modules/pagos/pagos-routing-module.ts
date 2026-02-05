8f14844517c78a1df8f6d9134641852ed20b158f
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2026-02-05 18:22:52 -0500

    Agrega estilos generales a la app angular 21

--- sistema-pagos-frontend/src/app/modules/pagos/pagos-routing-module.ts
+++ sistema-pagos-frontend/src/app/modules/pagos/pagos-routing-module.ts
@@ -2,24 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { Rol } from '../../core/models/auth.model';
+import { PagoFormComponent } from './components/pago-form/pago-form.component';
+import { authGuard } from '../../core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: "",
     component: PagosListComponent,
-    canActivate: [ AuthGuard, RoleGuard ],
+    canActivate: [ authGuard, RoleGuard ],
     data: { roles: [Rol.ADMIN, Rol.ESTUDIANTE, Rol.FINANZAS]}
   },
   {
     path: "nuevo",
     component: PagoFormComponent,
-    canActivate: [ AuthGuard, RoleGuard ],
+    canActivate: [ authGuard, RoleGuard ],
     data: { roles: [Rol.ADMIN, Rol.FINANZAS, Rol.ESTUDIANTE]}
   },
   {
     path: ":id",
     component: PagoDetailComponent,
-    canActivate: [ AuthGuard, RoleGuard ],
+    canActivate: [ authGuard, RoleGuard ],
     data: { roles: [Rol.ADMIN, Rol.ESTUDIANTE, Rol.FINANZAS]}
   }
 ]
